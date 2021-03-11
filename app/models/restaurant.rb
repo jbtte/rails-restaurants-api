38365f61a0f546e7e5cf6973ff43bbff1320225d
Joao Baumotte <jbaumotte@gmail.com>
2021-03-11 11:54:02 -0300

    Initial commit

---
+++ app/models/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant < ApplicationRecord
+  has_many :comments
+  belongs_to :user
+  validates :name, :address, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying Restaurant DB"
+Restaurant.destroy_all
+User.destroy_all
+
+users = []
+puts 'Creating Users'
+5.times do |each|
+  email = Faker::Internet.email
+  password = 123456
+  user = User.create!(email: email, password: password)
+  users << user
+end
+
+puts 'Creating Restaurants'
+10.times do |each|
+  name = Faker::Name.name
+  address = Faker::Address.full_address
+  user = users.sample
+
+  Restaurant.create!(name:name, address:address, user:user)
+end
